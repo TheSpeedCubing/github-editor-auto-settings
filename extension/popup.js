c4e33a3c955c84fb252725664c2fc2c83dfc3335
TheSpeedCubing <speedcubing@speedcubing.top>
2025-12-06 02:29:29 +0800

    feat: close menu after save

--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,23 @@
-const indentMode = document.getElementById('indentMode');
-const indentSize = document.getElementById('indentSize');
-const lineWrap = document.getElementById('lineWrap');
-const saveBtn = document.getElementById('saveBtn');
+document.addEventListener('DOMContentLoaded', () => {
+    const indentMode = document.getElementById('indentMode');
+    const indentSize = document.getElementById('indentSize');
+    const lineWrap = document.getElementById('lineWrap');
+    const saveBtn = document.getElementById('saveBtn');
 
-chrome.storage.sync.get(['indentMode', 'indentSize', 'lineWrap'], (data) => {
-  if (data.indentMode) indentMode.value = data.indentMode;
-  if (data.indentSize) indentSize.value = data.indentSize;
-  if (data.lineWrap) lineWrap.value = data.lineWrap;
-});
+    chrome.storage.sync.get(['indentMode', 'indentSize', 'lineWrap'], (data) => {
+        if (data.indentMode) indentMode.value = data.indentMode;
+        if (data.indentSize) indentSize.value = data.indentSize;
+        if (data.lineWrap) lineWrap.value = data.lineWrap;
+    });
 
-saveBtn.addEventListener('click', () => {
-  chrome.storage.sync.set({
-    indentMode: indentMode.value,
-    indentSize: indentSize.value,
-    lineWrap: lineWrap.value
-  }, () => {
-    alert('saved!!!!!');
-  });
+    saveBtn.addEventListener('click', () => {
+        chrome.storage.sync.set({
+            indentMode: indentMode.value,
+            indentSize: indentSize.value,
+            lineWrap: lineWrap.value
+        }, () => {
+			alert("Saved!!!!!");
+            window.close();
+        });
+    });
 });
